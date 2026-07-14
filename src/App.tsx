e2f2d2c8dc8095d9ec8dfc666a7880cd0a077395
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-07-14 17:47:09 +0530

    feat: integrate ConversationList and ConversationDetails components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { useConversations } from "./hooks/useConversations";
+
 import ConversationList from "./components/conversation/ConversationList";
+import type { Conversation } from "./types/conversation";
+import ConversationDetails from "./components/conversation/ConversationDetails";
 
 function App() {
   const {data, isLoading, error} = useConversations();
+  const [selectedConversation, setSelectedConversation] = useState<Conversation | null>(null);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -20,6 +25,13 @@ function App() {
       
       <ConversationList
         conversations = {data ?? []}
+        onSelect = {setSelectedConversation}
+      />
+
+      <hr />
+
+      <ConversationDetails
+        conversation = {selectedConversation}
       />
     </>
   );
