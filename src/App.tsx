8b6f234534065cb8e3cfe7dbf064d6ff2c39e372
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-07-14 18:06:22 +0530

    feat: implement search functionality in ConversationList

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { useConversations } from "./hooks/useConversations";
 import ConversationList from "./components/conversation/ConversationList";
 import type { Conversation } from "./types/conversation";
 import ConversationDetails from "./components/conversation/ConversationDetails";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const {data, isLoading, error} = useConversations();
   const [selectedConversation, setSelectedConversation] = useState<Conversation | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -17,14 +19,29 @@ function App() {
     return <h1>Error: {error.message}</h1>;
   }
 
+  const filteredConversations = (data ?? []).filter((conversation)=>{
+    const search = searchTerm.toLowerCase();
+
+    return(
+      conversation.customerName.toLowerCase().includes(search) ||
+      conversation.subject.toLowerCase().includes(search) ||
+      conversation.customerEmail.toLowerCase().includes(search)
+    )
+  })
+
   console.log(data)
 
   return(
     <>
       <h1>Conversation Inbox</h1>
+
+      <SearchBar
+        searchTerm = {searchTerm}
+        onSearchChange = {setSearchTerm}
+      />
       
       <ConversationList
-        conversations = {data ?? []}
+        conversations = {filteredConversations}
         onSelect = {setSelectedConversation}
       />
 
